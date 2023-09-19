ce122b69d46eb4acd5986c22aa64e5a82ef1bc64
pauliepus <142883643+pauliepus@users.noreply.github.com>
2023-09-19 11:22:02 +0200

    Add project files.

---
+++ mathcalculus/mathcalc.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+#ifndef _iostream_
+#define _iostream_
+#endif // !_iostream_
+
+#pragma once
+
+using namespace std; 
+
+//int main()
+//{
+//	//allocate 4 integer variables, each with a different name.
+//	int testscore0{ 402 };
+//	int testscore1{ 321 };
+//	int testscore2{ 892 };
+//	int testscore3{ 277 };
+//
+//		int Average{ (testscore0,testscore1, testscore2, testscore3) / 4 };
+//
+//	std:cout << "The average of nothing is...." "\n"<< Average << '\n';
+//		return 0;
+//
+//}
+
+int factorial(int n) {
+	if (n == 1 || n == 0)
+		result = 1;
+	else {
+		result = n * factorial(n - 1);
+	}
+	return result;
+}
+int main() {
+	 
+
+
+	switch case() {
+
+	}
+
+
+	std::cin >> n;
+}
+
+// you could recursively add physics to several objects... right?
+
+
+
+//class Calculator
+//	std::array calkis
+//
+//{
+//	array calkis{
+//		unsigned [0, 1, 2, 3];
+//		unsigned [3, 2, 1, 0];
+//
+//		return 0;
+//	};
+//	
+//}
